e9db72831c1e9e424391a5cde0eb48996d78e9aa
Juan Pablo Cabrera <jpcq2002@yahoo.com.mx>
2025-04-01 16:23:37 -0600

    [ADD] Activity

--- compilers/stage-1/main.py
+++ compilers/stage-1/main.py
@@ -2,7 +2,8 @@ from Lexer import *
 import sys
 
 if __name__ == '__main__':
-	lexer = Lexer("test_cases/bad/prog1.txt")
+	lexer = Lexer("/Users/juanpablocabreraquiroga/Documents/Compilers/compilers/stage-1/test_cases/bad/prog1.txt")
+ 
 	
 	token = lexer.scan()
 	while token.tag != Tag.EOF:
